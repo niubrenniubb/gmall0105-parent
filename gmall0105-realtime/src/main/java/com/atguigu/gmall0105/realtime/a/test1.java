bdccb577d37d878006d8c4035b6eaa31ed8636cb
niubrenniubb <2385956582@qq.com>
2020-10-18 22:33:08 +0800

    test

--- gmall0105-realtime/src/main/java/com/atguigu/gmall0105/realtime/a/test1.java
+++ gmall0105-realtime/src/main/java/com/atguigu/gmall0105/realtime/a/test1.java
@@ -2,6 +2,6 @@ package com.atguigu.gmall0105.realtime.a;
 
 public class test1 {
     public static void main(String[] args) {
-        System.out.println("yyyy");
+        System.out.println("yyyyy");
     }
 }
