7142353baf52567a9a730818415012301b142ccc
niubrenniubb <2385956582@qq.com>
2020-10-18 22:24:50 +0800

    test

--- gmall0105-realtime/src/main/java/com/atguigu/gmall0105/realtime/a/test.java
+++ gmall0105-realtime/src/main/java/com/atguigu/gmall0105/realtime/a/test.java
@@ -3,5 +3,7 @@ package com.atguigu.gmall0105.realtime.a;
 public class test {
     public static void main(String[] args) {
         System.out.println("aaaa");
+        System.out.println("ffff");
+
     }
 }
