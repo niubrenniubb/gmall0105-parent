0e21b6da63d488c3dc929cbde14c739a23c388ce
niubrenniubb <2385956582@qq.com>
2020-10-18 22:34:33 +0800

    test

--- gmall0105-realtime/src/main/java/com/atguigu/gmall0105/realtime/a/test.java
+++ gmall0105-realtime/src/main/java/com/atguigu/gmall0105/realtime/a/test.java
@@ -4,6 +4,7 @@ public class test {
     public static void main(String[] args) {
         System.out.println("aaaa");
         System.out.println("ffff");
+        System.out.println("rrrr");
 
     }
 }
